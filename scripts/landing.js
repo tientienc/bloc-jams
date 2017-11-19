8e9b944d9a6802f6f155b7dc68cce6962a08989d
tientienc <tienc25@gmail.com>
2017-11-19 11:14:30 -0500

    Add events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,6 @@
-var animatePoints = function(){
-  var points = document.getElementsByClassName('point');
+var points = document.getElementsByClassName('point');
+
+var animatePoints = function(points){
   var revealPoints = function(i) {
       points[i].style.opacity = 1;
       points[i].style.transform = "scaleX(1) translateY(0)";
@@ -10,4 +11,18 @@ var animatePoints = function(){
       revealPoints(i);
   }
 };
-animatePoints();
+
+window.onload = function(){
+  if(window.innerHeight > 950){
+    animatePoints(points);
+  }
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event){
+    if(document.documentElement.scrollTop || document.body.scroll >= scrollDistance){
+      animatePoints(points);
+    }
+  });
+}
