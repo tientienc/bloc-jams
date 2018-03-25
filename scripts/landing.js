28610fc8a75b84ee6c9785a3d772b42903f5c79a
tientienc <tienc25@gmail.com>
2018-03-25 13:03:13 -0400

    refactor animatePoints to jQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,27 +1,23 @@
-var points = document.getElementsByClassName('point');
-
-var revealPoints = function(points) {
-    points.style.opacity = 1;
-    points.style.transform = "scaleX(1) translateY(0)";
-    points.style.msTransform = "scaleX(1) translateY(0)";
-    points.style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
+  var revealPoint = function() {
+     $(this).css({
+         opacity: 1,
+         transform: 'scaleX(1) translateY(0)'
+     });
+   };
+  $.each($('.point'), revealPoint);
 };
 
-var animatePoints = function(points){
-  forEach(points, revealPoints);
-};
-
-window.onload = function(){
-  if(window.innerHeight > 950){
-    animatePoints(points);
+$(window).load(function() {
+  if ($(window).height() > 950) {
+    animatePoints();
   }
 
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-  window.addEventListener('scroll', function(event){
-    if(document.documentElement.scrollTop || document.body.scroll >= scrollDistance){
-      animatePoints(points);
+  $(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
     }
   });
-}
+});
